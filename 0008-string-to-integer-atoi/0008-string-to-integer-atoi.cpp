c4d2705a3aa9f5adb0b42da7e64d1b2aef437058
CODERAKS11 <147265113+CODERAKS11@users.noreply.github.com>
2026-05-20 21:02:09 +0530

    Time: 3 ms (14.29%), Space: 10.3 MB (5.1%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        bool neg = false;
-        int ans = 0;
-        int i = 0;
-        int n = s.size();
-        while(s[i] == ' ') i++;
-        if(i < n && (s[i] == '+' || s[i] == '-')) {
-            if(s[i] == '-') neg = true;
-            i++;
-        }
-        if(i >= n || !isdigit(s[i])) return 0;
-        while(s[i] == '0') i++;
-        while(i < n){
-            int digit = s[i] - '0';
-            if(!isdigit(s[i])) break;
-            if(ans > (INT_MAX - digit) / 10) {
-                return neg ? INT_MIN : INT_MAX;
+    void solve(string s, int ind, int &sign, long long &ans){
+        if(ind == s.size() || !isdigit(s[ind])) return;
+        int digit = s[ind] - '0';
+
+        if(sign == 1){
+            if(ans > INT_MAX / 10 || (ans == INT_MAX/10 && digit > 7)){
+                ans = INT_MAX;
+                return;
             }
-            ans = ans * 10 + digit;
+        } else {
+            if(ans > INT_MAX / 10 || (ans == INT_MAX/10 && digit > 8 )){
+                ans = INT_MIN;
+                return;
+            }
+        }
+        ans = ans * 10 + digit;
+        solve(s,ind + 1, sign, ans);
+    }
+
+    int myAtoi(string s) {
+        long long ans = 0;
+        int  sign = 1;
+        int n = s.size();
+        int i = 0;
+        while(i < n && s[i] == ' ') i++;
+
+        if(i < n && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '-') sign = -1;
             i++;
         }
-        return neg ? -1 * ans : ans;
+
+        while(i < n && s[i] == '0') i++;
+        solve(s,i,sign,ans);
+        return (int) (ans * sign);
     }
 };
