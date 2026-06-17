713aac8a34f0278f3454aaee684e13e0803e68eb
CODERAKS11 <147265113+CODERAKS11@users.noreply.github.com>
2026-06-17 07:09:14 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            char c = s[i];
+            if( c == '(' || c == '[' || c == '{') st.push(c);
+            else{
+                if(!st.empty() && c == ')' && st.top() == '(') st.pop();
+                else if(!st.empty() && c == ']' && st.top() == '[') st.pop();
+                else if(!st.empty() && c == '}' && st.top() == '{') st.pop();
+                else return false;
+            }
+        }
+        return st.size() == 0;
+    }
+};
