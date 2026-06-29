fb7d41321ff8067448bee7fb223fc89170c85f16
CODERAKS11 <147265113+CODERAKS11@users.noreply.github.com>
2026-06-29 13:32:17 +0530

    Time: 6 ms (89.65%), Space: 9.3 MB (92.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int start = 0,maxLen = 1, n = s.size();
-        for(int c = 0; c < 2*n - 2; c++){
+        int n = s.size();
+        int maxLen = 1;
+        int start = 0;
+        for(int c = 0; c < 2 * n - 2; c++){
             int left = c / 2;
-            int right = left + (c%2);
+            int right = left + (c % 2);
             while(left >= 0 && right < n && s[left] == s[right]){
                 int len = right - left + 1;
                 if(len > maxLen){
-                    maxLen = len;
                     start = left;
+                    maxLen = len;
                 }
                 left--;
                 right++;
             }
-            
         }
-        return s.substr(start,maxLen);
+        return s.substr(start, maxLen);
     }
 };
