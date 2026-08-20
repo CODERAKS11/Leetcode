2b47d24512547390c7c8a74fb8927c7e5c16880f
CODERAKS11 <147265113+CODERAKS11@users.noreply.github.com>
2026-08-20 11:04:40 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.47%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     double power(double x, long long n){
-        if(n == 0) return 1.0;
+        if(n == 0) return 1;
         if(n == 1) return x;
         if(n % 2 == 0) return power(x * x, n / 2);
-        return x * power(x , n - 1);
+        else return x * power(x, n - 1);
     }
     double myPow(double x, int n) {
-        long long num = n;        
-        if(num < 0){
-            return (1.0 / power(x, -1 * num));
+        long long N = n;
+        if(n < 0){
+            N = -1 * N;
+            x = 1.0 / x;
         }
-        return power(x, num);
+        return power(x,N);
     }
 };
